d0e74ba23b1083f277a5ef35bdb420bfe3f53071
Jessebanaan <165685041+Jessebanaan@users.noreply.github.com>
2025-05-19 15:30:31 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ],
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
 client.once('ready', () => {
     console.log(`Bot is online als ${client.user.tag}`);
 });
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', message => {
     if (message.content === '!hallo') {
         message.channel.send('Hoi daar!');
     }
